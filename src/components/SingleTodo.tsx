c97dac168c7418d738444e4a313955ae8f14ac45
Vrund Shah <vrund.shah@growexx.com>
2023-04-13 22:49:33 +0530

    :bug: Working on One bug

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Todo } from '../model';
+
+type Props = {
+  todo: Todo[];
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo = ({todo, todos, setTodos} : Props) => {
+  return (
+    <div>SingleTodo</div>
+  )
+}
+
+export default SingleTodo
