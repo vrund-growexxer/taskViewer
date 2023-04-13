16a109473db3fba4e35635554573ccd6460bc282
Vrund Shah <vrund.shah@growexx.com>
2023-04-13 17:38:12 +0530

    :lipstick: Add Input Ui and Setup code for Task Viewer

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+  return (
+    <div className="App">
+      <span className="heading">Task Viewer</span>
+      <InputField todo={todo} setTodo={setTodo}/>
+    </div>
+  );
+}
+
+export default App;
