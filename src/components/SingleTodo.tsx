972f53864d429b48eae3dc0ac40756c126b587aa
Vrund Shah <vrund.shah@growexx.com>
2023-04-14 14:24:48 +0530

    :bug: focus bug solved

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo } from '../model';
 import { AiFillDelete, AiFillEdit }  from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -29,11 +29,18 @@ const SingleTodo = ({todo, todos, setTodos} : Props) => {
     setEdit(false)
   }
 
+  const inputRef = useRef<HTMLInputElement>(null)
 
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit])
+  
   return (
     <form className='todo__single' onSubmit={(e) => handleEdit(e, todo.id)}>
       {edit ? (
         <input 
+        ref={inputRef}
         value={editTodo} 
         onChange={(e) => setEditTodo(e.target.value)} 
         className='todos__single__text'/>
